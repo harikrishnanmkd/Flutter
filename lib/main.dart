6a03ba75227fe1c706d68076f03d47f5fa30eee4
harikrishnanmkd <harikrishnanharikrishnan4296@gmail.com>
2023-10-28 15:23:53 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(MaterialApp(home:Center (child: Text("Welcome to Flutter Workshop",style: TextStyle(color: Colors.green,decoration: TextDecoration.none),)),));
+}
